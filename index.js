d90cae740aa5290d53be9d652cf3b2d756372c13
Bryan Schneller <bryan.schneller@gmail.com>
2024-02-15 19:59:10 -0500

    Add inquirer information

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const sequelize = require('./config/connection');
 const inquirer = require('inquirer');
 
+const start = async() => {
+    const response = await inquirer.createPromptModule([
+        {
+            type: "list",
+            message: "choose an option below:",
+            name: "selection",
+            choices: [
+                {
+                    name: "view employees",
+                    value: "VIEW EMP"
+                },
+                {
+                    name: "view roles",
+                    value: "VIEW ROLE"
+                }
+            ]
+        }
+    ])
+}
+
 sequelize.sync({force: false}).then(() => {
     console.log("connection successful!")
 })
